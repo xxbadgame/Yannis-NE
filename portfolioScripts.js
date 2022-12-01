14f8b362bbcb590402b64cb7b5bcd0a8518a85b5
xxbadgame <yannis.nzue@gmail.com>
2022-12-01 16:05:53 +0100

    Advanced

--- portfolioScripts.js
+++ portfolioScripts.js
@@ -1,3 +1,12 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+var map = L.map('map').setView([45.764043, 4.835659], 3);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([45.764043, 4.835659]).addTo(map);
+
+
+window.onscroll = function() {scrollFunction()};
+
