e5dbb4a41285e03255b72c006788c8aae1d378e8
xxbadgame <yannis.nzue@gmail.com>
2022-12-05 13:33:24 +0100

    bootstrap

--- portfolioScripts.js
+++ portfolioScripts.js
@@ -6,26 +6,3 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([45.764043, 4.835659]).addTo(map);
-
-
-var modal = document.getElementById("myModal");
-var body = document.body
-var btnModal = document.getElementById("btnModal");
-var span = document.getElementsByClassName("close")[0];
-
-btnModal.onclick = function(){
-    modal.style.display = "block";
-    body.style.overflowY = "hidden"
-}
-
-span.onclick = function(){
-    modal.style.display = "none";
-    body.style.overflowY = "visible"
-}
-
-window.onclick = function(event) {
-    if (event.target == modal){
-        modal.style.display = "none";
-        body.style.overflowY = "visible"
-    }
-}
